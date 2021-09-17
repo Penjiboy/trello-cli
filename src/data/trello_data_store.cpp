c73789b40197a0d08129e20f64cd55d0d76ba403
Penjani Chavula <chavulapenjani@gmail.com>
2021-09-16 23:08:17 -0700

    able to read the trello key and token

--- src/data/trello_data_store.cpp
+++ src/data/trello_data_store.cpp
@@ -4,6 +4,37 @@
 
 #include "httplib.h"
 
+#include <iostream>
+#include <fstream>
+
+TrelloDataStore::TrelloDataStore()
+{
+    // Read and set the key and token
+    std::fstream keyFile;
+    keyFile.open(kPathToKey, std::ios::in); // open a file to read
+    if (keyFile.is_open())
+    {
+        std::string tp;
+        while (std::getline(keyFile, tp)) 
+        {
+            m_key = tp;
+        }
+    }
+    keyFile.close();
+
+    std::fstream tokenFile;
+    tokenFile.open(kPathToToken, std::ios::in);
+    if (tokenFile.is_open())
+    {
+        std::string tp;
+        while (std::getline(tokenFile, tp))
+        {
+            m_token = tp;
+        }
+    }
+    tokenFile.close();
+}
+
 std::shared_ptr<TrelloDataStore> TrelloDataStore::GetInstance()
 {
     static std::shared_ptr<TrelloDataStore> instance(new TrelloDataStore());
@@ -14,9 +45,9 @@ std::shared_ptr<const std::vector<Board>> TrelloDataStore::GetAllBoards()
 {
     // Send HTTP Request
     httplib::Client client("https://www.google.com");
-    auto response = client.Get("/");
-    std::cout << "Response Status: " << response->status << std::endl;
-    std::cout << "Response Body: " << response->body << std::endl;
+    // auto response = client.Get("/");
+    // std::cout << "Response Status: " << response->status << std::endl;
+    // std::cout << "Response Body: " << response->body << std::endl;
 
     // Parse Response
 
--- src/data/trello_data_store.h
+++ src/data/trello_data_store.h
@@ -13,6 +13,11 @@ public:
 
 private:
     /* data */
-    TrelloDataStore(/* args */) {}
+    TrelloDataStore(/* args */);
     const std::string kUrlBase = "https://api.trello.com/1";
+    const std::string kPathToKey = ".config/developer_api_key.txt";
+    const std::string kPathToToken = ".config/developer_api_token.txt";
+
+    std::string m_key;
+    std::string m_token;
 };
