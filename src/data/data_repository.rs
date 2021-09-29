0cb1b11f8527f8f7cad082fb87e13632d062d31b
Penjani Chavula <chavulapenjani@gmail.com>
2021-09-28 22:16:37 -0700

    implement a basic level of caching

--- src/data/data_repository.rs
+++ src/data/data_repository.rs
@@ -7,9 +7,17 @@ use std::sync::Once;
 
 static INIT: Once = Once::new();
 
-#[derive(Clone)]
 pub struct DataRepository {
     trello_store: TrelloDataStore,
+    active_board: Option<Board>,
+    active_boardlist: Option<BoardList>,
+    active_card: Option<Card>,
+    active_checklist: Option<CardChecklist>,
+
+    cache_boards: Option<Vec<Board>>,
+    cache_boardlists: Option<Vec<Board>>,
+    cache_cards: Option<Vec<Card>>,
+    cache_checklists: Option<Vec<CardChecklist>>,
 }
 
 impl DataRepository {
@@ -19,6 +27,14 @@ impl DataRepository {
             let trello = TrelloDataStore::new(None, None); // TODO: Change this
             dr = Some(DataRepository {
                 trello_store: trello,
+                active_board: None,
+                active_boardlist: None,
+                active_card: None,
+                active_checklist: None,
+                cache_boards: None,
+                cache_boardlists: None,
+                cache_cards: None,
+                cache_checklists: None,
             });
         });
 
@@ -34,6 +50,21 @@ pub trait DataStore {
 #[async_trait]
 impl DataStore for DataRepository {
     async fn get_all_boards(&mut self) -> Result<Vec<Board>, Box<dyn std::error::Error>> {
-        self.trello_store.get_all_boards().await
+        let boards_result: Result<Vec<Board>, Box<dyn std::error::Error>> =
+            self.trello_store.get_all_boards().await;
+        match boards_result {
+            Ok(boards) => {
+                self.cache_boards = Some(boards.clone());
+                Ok(boards)
+            }
+
+            Err(why) => {
+                if self.cache_boards.is_none() {
+                    Err(why)
+                } else {
+                    Ok(self.cache_boards.clone().unwrap())
+                }
+            }
+        }
     }
 }
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -3,16 +3,19 @@ use datetime;
 pub mod data_repository;
 mod trello_data_store;
 
+#[derive(Clone)]
 pub struct ID {
     pub trello_id: Option<String>,
     pub local_id: Option<String>,
 }
 
+#[derive(Clone)]
 pub struct CardLabel {
     pub name: String,
     pub color: String,
 }
 
+#[derive(Clone)]
 pub struct CardComment {
     pub id: ID,
     pub text: String,
@@ -20,18 +23,21 @@ pub struct CardComment {
     pub comment_time: datetime::LocalDateTime,
 }
 
+#[derive(Clone)]
 pub struct CardChecklistTask {
     pub id: ID,
     pub name: String,
     pub is_complete: bool,
 }
 
+#[derive(Clone)]
 pub struct CardChecklist {
     pub id: ID,
     pub name: String,
     pub tasks: Vec<CardChecklistTask>,
 }
 
+#[derive(Clone)]
 pub struct Card {
     pub id: ID,
     pub name: String,
@@ -42,11 +48,13 @@ pub struct Card {
     pub checklists: Vec<CardChecklist>,
 }
 
+#[derive(Clone)]
 pub struct CardDueDate {
     pub card: Card,
     pub due_date: datetime::LocalDateTime,
 }
 
+#[derive(Clone)]
 pub struct BoardList {
     pub id: ID,
     pub name: String,
@@ -54,6 +62,7 @@ pub struct BoardList {
     pub cards: Vec<Card>,
 }
 
+#[derive(Clone)]
 pub struct Board {
     pub id: ID,
     pub name: String,
